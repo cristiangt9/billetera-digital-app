0cdd5297b2779a9d99be6d4c83214e6cc6690306
Cristian Gonzalez <cristiangt9@gmail.com>
2021-05-01 23:07:38 -0500

    Se agrega la funcionalidad de recoger los datos del pago y se envian al backed y se recibe respuesta satisfacctoria

---
+++ src/components/pago/SolicitudPagoForm.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import useAxioshttp from "../../hooks/use-axioshttp";
+import Card from "../ui/Card";
+import LoadingSpinner from "../ui/LoadingSpinner";
+
+import classes from "./SolicitudPagoForm.module.css";
+
+const SolicitudPagoForm = (props: any) => {
+
+  const { isLoading, error, sendRequest } = useAxioshttp();
+  const processData = (response: any): void => {
+    props.onResponse();
+  };
+
+  const [form, setForm] = useState({
+    documento: "",
+    celular: "",
+    valor: "",
+  });
+
+  function submitFormHandler(event: any) {
+    event.preventDefault();
+
+    // validate here
+
+      sendRequest(
+        {
+          method: "POST",
+          url: "transacciones",
+          data: {tipo: 'solicitudPago',...form, token: props.token},
+        },
+        processData
+      );
+  }
+
+  const changeHandler = (event: any) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      [event.target.name]: event.target.value,
+    }));
+  };
+  if (error.onError)
+    return <section className="centered">{props.error.message}</section>;
+  return (
+    <>
+      <Card>
+        <form className={classes.form} onSubmit={submitFormHandler}>
+          {isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor="documento">Documento Receptor del Pago</label>
+            <input
+              type="text"
+              id="documento"
+              name="documento"
+              onChange={changeHandler}
+              value={form.documento}
+              disabled={props.enviado}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="celular">Celular Receptor del Pago</label>
+            <input
+              type="text"
+              id="celular"
+              name="celular"
+              onChange={changeHandler}
+              value={form.celular}
+              disabled={props.enviado}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="valor">Valor</label>
+            <input
+              type="text"
+              id="valor"
+              name="valor"
+              onChange={changeHandler}
+              value={form.valor}
+              disabled={props.enviado}
+            />
+          </div>
+
+          <div className={classes.actions}>
+            <button className="btn" disabled={props.enviado}>Pagar</button>
+          </div>
+        </form>
+      </Card>
+    </>
+  );
+};
+
+export default SolicitudPagoForm;
--- src/pages/Pagar.tsx
+++ src/pages/Pagar.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
+import SolicitudPagoForm from "../components/pago/SolicitudPagoForm";
+import Card from "../components/ui/Card";
 import { stateStore } from "../CustomInterface";
 import classes from "./Pagar.module.css";
 
 const Pagar = () => {
   const user = useSelector((state: stateStore) => state.user);
+  const [showInputTokenConfirm, setShowInputTokenConfirm] = useState(false);
   const history = useHistory();
+
+const SolicitudResponseHandler = () => {
+    setShowInputTokenConfirm(true);
+}
+
   if (user.token === "") history.push("/ingresar");
-  return <div className={classes.Pagar}>Pagar Page</div>;
+  
+  return (
+    <div className={classes.Pagar}>
+      <Card className={classes.saldoCard}>
+      <SolicitudPagoForm onResponse={SolicitudResponseHandler} enviado={showInputTokenConfirm} token={user.token}/>
+      {showInputTokenConfirm && <p>Ingrese Token</p>}
+      </Card>
+    </div>
+  );
 };
 
 export default Pagar;
